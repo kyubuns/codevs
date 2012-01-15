a5a86c63efedcc57fe6deed502d01d76eb24f1e9
kyubuns <jp@ex.cx>
2012-01-15 17:28:03 +0900

    デバッグ出力無くした

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -3,9 +3,11 @@
 
 void Logger::write(std::string message)
 {
+	/*
 	if(message.size()==1 && message[0]>='a' && message[0]<='z') return;
 	std::ofstream ofs( "log.txt", std::ios::app);
 	ofs << message;
+	*/
 }
 
 void Logger::out()
